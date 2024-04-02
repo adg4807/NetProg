e87c2880424419ad1039943209106060cf1be7f8
lee_JA <81452897+adg4807@users.noreply.github.com>
2024-04-02 11:36:33 +0900

    Update map_lambda.py

--- map_lambda.py
+++ map_lambda.py
@@ -1,3 +1,10 @@
+def Squares(n):
+    return n**2
+numbers =[1,3,5,9]
+print(list(map(Squares,numbers)))
+
 Squares=lambda n:n**2
 numbers=[1,3,5,9]
 print(list(map(Squares,numbers)))
+
+print(list(map(lambda n:n**2,[1,3,5,9])))
